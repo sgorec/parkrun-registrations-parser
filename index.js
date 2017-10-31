137aa8438ec8d8f4bd74008a39d5508b2588f9ce
Denis Lukyanov <dlook@toscale.co>
2017-10-31 21:22:10 +0200

    Remove log

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ var request = require('request');
 var fs = require('fs');
 var path = require('path');
 
-console.log('>> Load content descriptors.');
-
 var contentDescriptors = fs.readFileSync(path.join(__dirname, './content-descriptors.txt'))
 	.toString()
 	.split(/\n|\r/)
 	.filter(function(string) { return !!string.trim(); });
-
-console.log('<< Loaded %s descriptor(s).', contentDescriptors.length);
 
 var contentCheckers = contentDescriptors.map(function(descriptor) {
 	return new RegExp(escapeRegExp(descriptor.trim()), 'i');
@@ -20,16 +16,12 @@ function grabData($) {
 	var numberExtrator = /\D*(\d+)\D*/g;
 	var data = [];
 
-	console.log('>> Start grab data from html content.');
-
 	$('table.wikitable > tbody > tr').each(function(_, tr) {
 		var text = $(tr).text();            
 		var isMatch = contentCheckers.some(function(checker) { return checker.test(text); });
 
 		if (isMatch) {
 			var eventName = $(tr).find('a').text();
-
-			console.log('Match event "%s".', eventName);
 
 			data.push({ 
 				'Event': eventName,
@@ -38,8 +30,6 @@ function grabData($) {
 			});
 		}
 	});
-
-	console.log('<< End grab data from html content.');
 
 	return data;
 }
@@ -52,18 +42,11 @@ function leadZero(num) {
 	return (num < 10 ? '0' : '') + num;
 }
 
-console.log('>> Load html content.');
-
 request('http://wiki.parkrun.info/index.php/Registrations_this_Week', function(err, resp, body) {
-	console.log('<< Loaded html content.');
-	
 	var $body = cheerio.load(body);
 	var data = grabData($body);
 
-	console.log('>> Save data to files.');
-
 	if (!data || !data.length) {
-		console.log('<< Nothing to save.');
 		return;
 	}
 
@@ -85,6 +68,4 @@ request('http://wiki.parkrun.info/index.php/Registrations_this_Week', function(e
 		fileName = path.join(__dirname, './out', fileName);
 		fs.appendFileSync(fileName, date + ' ' + event['Total'] + '\n');
 	});
-
-	console.log('<< Saved data to %s files.', data.length);
 });
